c5f2dbfb48b8b64618a81d50d998278b25f79edc
Rob Nugen <rob@robnugen.com>
2017-12-20 15:58:59 -0300

    Add mouse entry/exit listeners

--- subscenes/Tile.gd
+++ subscenes/Tile.gd
@@ -10,3 +10,11 @@ func _ready():
 
 func _on_Area2D_input_event( viewport, event, shape_idx ):
 	print("that happened")
+
+
+func _on_Area2D_mouse_enter():
+	print("mouse entered")
+
+
+func _on_Area2D_mouse_exit():
+	print("mouse exited")
