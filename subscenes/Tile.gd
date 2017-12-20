a8c56468d74f728477af0fb0205e14fa258014f5
Rob Nugen <rob@robnugen.com>
2017-12-20 15:55:32 -0300

    Area2D with input_event

---
+++ subscenes/Tile.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	print("I am a Tile")
+	set_process_input(true)
+
+func _on_Area2D_input_event( viewport, event, shape_idx ):
+	print("that happened")
