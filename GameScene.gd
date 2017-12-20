7a807e1c4d26ee5dbb847fa5dd2b0e4799dcf3eb
Rob Nugen <rob@robnugen.com>
2017-12-20 18:39:36 -0300

    Instantiate level/normal_welcome_01.gd

--- GameScene.gd
+++ GameScene.gd
@@ -1,10 +1,21 @@
 extends Node2D
 
 var Tile = preload("res://SubScenes/Tile.tscn")
+const level_format = "res://levels/%s_%s_%02d.gd"		# normal_welcome_01
 
 func _ready():
 	print("Started Game Scene")
+	start_level(1)
 	new_player()
+
+func start_level(level_num):
+	var level_difficulty = "normal"
+	var level_group = "welcome"
+	var level_name = level_format % [level_difficulty, level_group, level_num]
+	print("starting Level ", level_name)
+
+	var current_level = load(level_name).new()		# load() gets a GDScript and new() instantiates it
+	print(current_level.width)
 
 func new_player():
 	# new player will be a random of four colors
---
+++ levels/normal_welcome_01.gd
@@ -0,0 +1,4 @@
+extends "NormalLevel.gd"
+
+func _ready():
+	width = 5   # this does not work, perhaps it's a race condition
