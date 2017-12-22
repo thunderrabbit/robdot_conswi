b0afb4ca84eafed1818b0c1df3f9f3e6c363615b
Rob Nugen <rob@robnugen.com>
2017-12-22 00:52:48 -0300

    SteeringPad can detect left and right mouse clicks

--- subscenes/SteeringPad.gd
+++ subscenes/SteeringPad.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
 func _on_Area2D_input_event( viewport, event, shape_idx ):
-	print(event)
-
-
+	if event.type == InputEvent.MOUSE_BUTTON \
+	and event.button_index == BUTTON_LEFT \
+	and event.pressed:
+		if event.pos.x < self.get_pos().x:
+			print("left")
+		else:
+			print("right")
