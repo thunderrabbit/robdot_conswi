9a3c08aade907dea95ce533703f6547b30308ca2
Rob Nugen <rob@robnugen.com>
2017-12-22 21:40:50 -0300

    Remove unneeded comments

--- subscenes/Tile.gd
+++ subscenes/Tile.gd
@@ -25,17 +25,17 @@ func _on_Area2D_input_event( viewport, event, shape_idx ):
 	if event.type == InputEvent.MOUSE_BUTTON \
 	and event.button_index == BUTTON_LEFT:
 		if event.pressed:
-			print("mouse clicked ", Helpers.pixels_to_slot(get_pos()))
+#			print("mouse clicked ", Helpers.pixels_to_slot(get_pos()))
 			emit_signal("clicked", Helpers.pixels_to_slot(get_pos()), tile_type)
 		else: # not event.pressed:
 			print("mouse unclicked ", Helpers.pixels_to_slot(get_pos()))
 			emit_signal("unclicked", Helpers.pixels_to_slot(get_pos()), tile_type)
 
 func _on_Area2D_mouse_enter():
-	print("mouse entered ", Helpers.pixels_to_slot(get_pos()))
+#	print("mouse entered ", Helpers.pixels_to_slot(get_pos()))
 	emit_signal("entered", Helpers.pixels_to_slot(get_pos()), tile_type)
 
 
 func _on_Area2D_mouse_exit():
-	print("mouse exited ", Helpers.pixels_to_slot(get_pos()))
+#	print("mouse exited ", Helpers.pixels_to_slot(get_pos()))
 	emit_signal("exited", Helpers.pixels_to_slot(get_pos()), tile_type)
