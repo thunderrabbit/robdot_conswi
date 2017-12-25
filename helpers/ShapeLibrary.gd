89654e9b826eb240140c3082ef990017f521d27e
Rob Nugen <rob@robnugen.com>
2017-12-25 10:15:02 -0300

    Find width+height of a swipe with ShapeLibrary

---
+++ helpers/ShapeLibrary.gd
@@ -0,0 +1,30 @@
+extends Node
+
+func _ready():
+	givenSwipe_showArray([Vector2(3,14),Vector2(3,15),Vector2(4,15)])
+	
+func givenSwipe_showArray(swipeCoordinates):
+	var swipeDimensions = getSwipeDimensions(swipeCoordinates)
+	print(swipeDimensions)
+
+# given coordinates of a swipe, return a Vector2
+# of their width and height
+func getSwipeDimensions(swipeCoordinates):
+	# start somewhere and call it extreme
+	var leftmost_x = swipeCoordinates[0].x
+	var rightmost_x = swipeCoordinates[0].x
+	var upmost_y = swipeCoordinates[0].y
+	var downmost_y = swipeCoordinates[0].y
+
+	
+	for coord in swipeCoordinates:
+		leftmost_x = min(leftmost_x,coord.x)
+		rightmost_x = max(rightmost_x,coord.x)
+		upmost_y = min(upmost_y,coord.y)
+		downmost_y = max(downmost_y, coord.y)
+	
+	# see how wide/tall the swipe is
+	var width = (rightmost_x - leftmost_x) + 1
+	var height = (downmost_y - upmost_y) + 1
+	return Vector2(width,height)
+
