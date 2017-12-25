3c1aff48914287ea5a83576977a026e28027d843
Rob Nugen <rob@robnugen.com>
2017-12-25 11:42:57 -0300

    Print bitArray for each swipe we make
    This will help create shapes in ShapeLibrary

--- GameScene.gd
+++ GameScene.gd
@@ -228,6 +228,7 @@ func piece_unclicked():
 		for pos in swipe_array:
 			Helpers.board[pos].unhighlight()
 	else:
+		ShapeLibrary.givenSwipe_showArray(swipe_array)
 		for pos in swipe_array:
 			if Helpers.board[pos] != null:
 				Helpers.board[pos].remove_yourself()
