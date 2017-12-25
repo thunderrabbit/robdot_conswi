a3f12165645ef5857c9d1bafce5fb40348d0d32d
Rob Nugen <rob@robnugen.com>
2017-12-25 10:51:03 -0300

    Removed hardcoded top left corner of testswipe

--- helpers/ShapeLibrary.gd
+++ helpers/ShapeLibrary.gd
@@ -8,8 +8,7 @@ func givenSwipe_showArray(swipeCoordinates):
 	print(swipeDimensions)
 	createBitmap(swipeDimensions,swipeCoordinates)
 
-# given coordinates of a swipe, return a Vector2
-# of their width and height
+# given coordinates of a swipe, return its width, height, top left, bottom right
 func getSwipeDimensions(swipeCoordinates):
 	# start somewhere and call it extreme
 	var leftmost_x = swipeCoordinates[0].x
@@ -17,32 +16,34 @@ func getSwipeDimensions(swipeCoordinates):
 	var upmost_y = swipeCoordinates[0].y
 	var downmost_y = swipeCoordinates[0].y
 
-	
+
 	for coord in swipeCoordinates:
 		leftmost_x = min(leftmost_x,coord.x)
 		rightmost_x = max(rightmost_x,coord.x)
 		upmost_y = min(upmost_y,coord.y)
 		downmost_y = max(downmost_y, coord.y)
-	
+
 	# see how wide/tall the swipe is
 	var width = (rightmost_x - leftmost_x) + 1
 	var height = (downmost_y - upmost_y) + 1
-	return Vector2(width,height)
+
+	return {'topleft': Vector2(leftmost_x,upmost_y),
+			'botright': Vector2(rightmost_x, downmost_y),
+			'width': width,
+			'height': height}
 
 func createBitmap(swipeDimensions,swipeCoordinates):
 	var bitmapArray = []
-	var width = swipeDimensions.x
-	var height = swipeDimensions.y
+	var width = swipeDimensions.width			# int
+	var height = swipeDimensions.height			# int
+	var upperleft = swipeDimensions.topleft		# Vector2
+	var tc = Vector2(0,0)	# translated_coordinates
 
 	# fill array with zeroes
 	for i in range(width * height):
 		bitmapArray.append(0)
 
-	# hardcode until refactor above
-	var upperleft = Vector2(3,14)
-	var tc = Vector2(0,0)	# translated_coordinates
 	for coord in swipeCoordinates:
 		tc = coord - upperleft
 		bitmapArray[tc.x + tc.y * width] = 1
 	print(bitmapArray)
-
