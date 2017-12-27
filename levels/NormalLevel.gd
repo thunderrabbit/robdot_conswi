04f7652326f3437eb6f2b9d0b294a81b68b2ce00
Rob Nugen <rob@robnugen.com>
2017-12-27 10:51:12 -0300

    nice round number

--- levels/NormalLevel.gd
+++ levels/NormalLevel.gd
@@ -5,6 +5,7 @@ var height = 10				# Game tiles tall
 var fill_level = false		# true half fills level with tiles
 var gravity_timeout = 1		# seconds, tile moves down
 var min_swipe_len = 3		# higher is harder
+var max_tiles_avail = 32768	# including tiles used in fill_level = true
 var debug_level = 0			# boolean for now
 
 var queue_len = 3			# queue is upcoming tiles
