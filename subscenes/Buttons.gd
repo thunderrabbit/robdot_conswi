bb336e278960bd02d1657c6837f6e20e7294e744
Rob Nugen <rob@robnugen.com>
2017-12-27 17:40:08 -0200

    Tell Buttons.gd how to display the BetweenLevel Buttons

--- subscenes/Buttons.gd
+++ subscenes/Buttons.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var SteeringPad = preload("res://SubScenes/SteeringPad.tscn")
+var EndLevelBut = preload("res://SubScenes/LevelEndedButtons.tscn")
 var parent_scene
 
 func set_game_scene(game_scene):
@@ -11,3 +12,8 @@ func add_steering_pad():
 	steering_pad.set_pos(Helpers.steering_pad_pixels())
 	steering_pad.set_game_scene(parent_scene)
 	parent_scene.add_child(steering_pad)
+
+func display_level_end_buttons():
+	var endLevelButtons = EndLevelBut.instance()
+	endLevelButtons.set_pos(Vector2(0,0))
+	parent_scene.add_child(endLevelButtons)
