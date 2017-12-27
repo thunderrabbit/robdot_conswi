3c63b16d9f4f0e207d29f6a90609864d211c1e30
Rob Nugen <rob@robnugen.com>
2017-12-27 10:01:49 -0300

    Add comments

--- levels/NormalLevel.gd
+++ levels/NormalLevel.gd
@@ -1,14 +1,15 @@
 extends Node
 
-var width = 7
-var height = 10
-var fill_level = false
-var gravity_timeout = 1
-var min_swipe_len = 3
-var debug_level = 0
+var width = 7				# Game tiles across
+var height = 10				# Game tiles tall
+var fill_level = false		# true half fills level with tiles
+var gravity_timeout = 1		# seconds, tile moves down
+var min_swipe_len = 3		# higher is harder
+var debug_level = 0			# boolean for now
 
-var queue_len = 3
+var queue_len = 3			# queue is upcoming tiles
 
+# really no reason for these???
 func level_width():
 	return width
 
