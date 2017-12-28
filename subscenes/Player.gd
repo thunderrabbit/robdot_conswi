6efdc6ec095f7ef0a35ee109704490b89ba2e739
Rob Nugen <rob@robnugen.com>
2017-12-28 14:08:55 -0200

    Player will use mytile and myshadow
    instead of array tile_y_shadow
    
    because I think the array was causing memory issues

--- subscenes/Player.gd
+++ subscenes/Player.gd
@@ -10,24 +10,20 @@ var should_show_shadow = false
 var nailed = false
 
 func _ready():
-	# array to hold both parts of our player
-	tile_y_shadow = []
 	set_process_input(false)
 
 func set_type(new_tile_type_ordinal):
-	# instantiate 1 Tile each for our player and shadow.  i is unused here
-	for i in range(2):
-		# insantiate new Tile to be our player
-		var tile = Tile.instance()
-		tile.set_tile_type(new_tile_type_ordinal)
-		# keep it in player_sprites so we can find them later
-		tile_y_shadow.append(tile)
+	# instantiate 1 Tile each for our player and shadow.
+	mytile = Tile.instance()
+	mytile.set_tile_type(new_tile_type_ordinal)
+	# add Tile to scene
+	parent_scene.add_child(mytile)
 
-		# add Tile to scene
-		parent_scene.add_child(tile)
-
+	myshadow = Tile.instance()
+	myshadow.set_tile_type(new_tile_type_ordinal)
 	# remove collider from shadow so it ignores mouse
-	tile_y_shadow[1].get_node("CollisionShape2D").queue_free()
+	myshadow.get_node("CollisionShape2D").queue_free()
+	parent_scene.add_child(myshadow)
 
 func set_game_scene(game_scene):
 	parent_scene = game_scene
@@ -35,22 +31,22 @@ func set_game_scene(game_scene):
 # update player sprite display
 func set_position(player_position):
 	my_position = player_position
-	tile_y_shadow[0].set_pos(Helpers.slot_to_pixels(player_position))
+	mytile.set_pos(Helpers.slot_to_pixels(player_position))
 	if not nailed:
-		tile_y_shadow[1].set_pos(Helpers.slot_to_pixels(Vector2(player_position.x, column_height(player_position.x))))   ## shadow
-		var shadow = tile_y_shadow[1].get_node("TileSprite")
-		if shadow != null:
+		myshadow.set_pos(Helpers.slot_to_pixels(Vector2(player_position.x, column_height(player_position.x))))   ## shadow
+	#	var shadowsprite = myshadow.get_node("TileSprite")
+		if myshadow.my_sprite != null:
 			if should_show_shadow:
-				shadow.show()
-				shadow.set_modulate(Color(1,1,1, 0.3))
+				myshadow.my_sprite.show()
+				myshadow.my_sprite.set_modulate(Color(1,1,1, 0.3))
 			else:
-				shadow.hide()
+				myshadow.my_sprite.hide()
 
 # player has been nailed so it should animate or whatever
 func nail_player():
 	# remove player's shadow
-	tile_y_shadow[0].become_swipable()
-	tile_y_shadow[1].get_node("TileSprite").queue_free()
+	mytile.become_swipable()
+	myshadow.queue_free()
 	nailed = true
 	pass
 	
@@ -75,12 +71,11 @@ func set_show_shadow(should_i):
 	set_position(my_position)
 
 func highlight():
-	tile_y_shadow[0].my_sprite.highlight()
+	mytile.my_sprite.highlight()
 
 func unhighlight():
-	tile_y_shadow[0].my_sprite.unhighlight()
+	mytile.my_sprite.unhighlight()
 
 func remove_yourself():
 	Helpers.board[my_position] = null
-	tile_y_shadow[0].queue_free()
-	tile_y_shadow[1].queue_free()		# release shadow
+	queue_free()		# release yourself
