c60ed18fdbc1b04abe736c0edb96809fe5ab9130
Rob Nugen <rob@robnugen.com>
2017-12-28 20:32:52 -0200

    Add Global consts: tile types and level completions

--- helpers/Globals.gd
+++ helpers/Globals.gd
@@ -6,3 +6,13 @@ const GLOBALleft_space = 10		# Space on the left
 const GLOBALslot_gap_v = 5
 const GLOBALslot_gap_h = 5
 const SLOT_SIZE = 52
+
+const LEVEL_WIN 		= 1
+const LEVEL_NO_ROOM		= 2
+const LEVEL_NO_TIME		= 3
+const LEVEL_NO_TILES	= 4
+
+const TYPE_DOG			= 0
+const TYPE_CAT			= 1
+const TYPE_PIG			= 2
+const TYPE_SHEEP		= 3
