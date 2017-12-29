41e7d64b45a9261abd76115f7c3a266cd13bbd3c
Rob Nugen <rob@robnugen.com>
2017-12-28 22:41:42 -0200

    Allow the level end screen to display reason for end

--- subscenes/LevelEndedButtons.gd
+++ subscenes/LevelEndedButtons.gd
@@ -10,5 +10,16 @@ func _ready():
 	# Initialization here
 	pass
 
+func level_over_reason(reason):
+	print(reason)
+	if reason == G.LEVEL_WIN:
+		get_node("LevelOverTitle").set_texture(preload("res://images/buttons/you_win.png"))
+	if reason == G.LEVEL_NO_ROOM:
+		get_node("LevelOverTitle").set_texture(preload("res://images/buttons/no_room.png"))
+	if reason == G.LEVEL_NO_TIME:
+		get_node("LevelOverTitle").set_texture(preload("res://images/buttons/time_up.png"))
+	if reason == G.LEVEL_NO_TILES:
+		get_node("LevelOverTitle").set_texture(preload("res://images/buttons/no_tiles.png"))
+
 func _on_TryAgain_pressed():
 	game_scene.requested_replay_level()
