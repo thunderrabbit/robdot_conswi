c6c8993b8549ad8fa6cde83eaf3c9a9941677106
Rob Nugen <rob@robnugen.com>
2017-12-29 17:09:23 -0200

    Add 50 buttons to LevelScene
    Fixes #24

--- LevelScene.gd
+++ LevelScene.gd
@@ -6,23 +6,31 @@ extends Node2D
 # here so it is clear that they are only used here.
 # Feel free to arrange these buttons in a better way.
 
-const SLOT_SIZE       = 50
-var left_space        = 0
-var top_space         = 0
-var slot_gap          = 15
+const SLOT_SIZE       = 70
+var left_space        = 30
+var top_space         = 30
+var slot_gap          = 35
 var slot_gap_h        = slot_gap
 var slot_gap_v        = slot_gap
 var buttons_across    = 5
 
 func _ready():
-    # Called every time the node is added to the scene.
-    # Initialization here
-    pass
+    var button_loc = Vector2(0,0)
+    for level in range(50):
+        button_loc = level_to_pixels(level)
+        print(button_loc)
+        var level_but = Button.new()
+        level_but.set_pos(button_loc)
+        level_but.set_text(String(level))
+        level_but.set_size(Vector2(SLOT_SIZE,SLOT_SIZE))
+        level_but.connect("pressed",self,"_on_Button_pressed",[level])
+        add_child(level_but)
 
 func level_to_pixels(level):
-    var slot = Vector2(level / buttons_across, level % buttons_across)
+    var slot = Vector2(level % buttons_across, level / buttons_across)
     return Vector2(left_space+(SLOT_SIZE + slot_gap_h)*(slot.x), 
                     top_space+(SLOT_SIZE + slot_gap_v)*(slot.y))
 
-func _on_Button_pressed():
+func _on_Button_pressed(level):
+    Helpers.requested_level = level
     get_node("/root/SceneChanger").goto_scene("res://GameScene.tscn")
